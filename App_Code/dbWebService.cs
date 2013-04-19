4aa17540becc6a269a28ed4049b1512879675741
Michael Dabydeen <mdabydeen@gmail.com>
2013-04-19 07:11:46 -0400

    adding everything else

---
+++ App_Code/dbWebService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data.SqlClient;
+using System.Configuration;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+
+public class dbWebService : System.Web.Services.WebService {
+
+    [WebMethod]
+    public string Details(int sID) {
+
+        //connect to the database 
+        String connStr = ConfigurationManager.ConnectionStrings["connStr_wsTest3"].ConnectionString;
+        SqlConnection conn = new SqlConnection(connStr);
+        conn.Open();
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = conn;
+        String SQL = "SELECT [CompanyName],[City],[Country] FROM [Suppliers] WHERE SupplierID = " + sID;
+        cmd.CommandText = SQL;
+        cmd.ExecuteNonQuery();
+        SqlDataReader dr = cmd.ExecuteReader();
+        if (dr.Read())
+        {
+            for (int i = 0; i < dr.FieldCount; i++)
+            {
+                return dr.GetValue(i).ToString();
+            }
+
+        }
+        conn.Close();
+
+        //to return - Company Name, City & Country
+        return "Hello World";
+    }
+    
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        localhost.dbWebService proxy = new localhost.dbWebService();
+
+        String name = proxy.Details(1);
+        txtLbl.Text = name;
+    }
+}
